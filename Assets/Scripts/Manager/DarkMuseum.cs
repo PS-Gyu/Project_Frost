ffcc7da9bdb467df55f39102ccadd433b763453d
Vimarone <44824410+Vimarone@users.noreply.github.com>
2020-05-17 14:09:06 +0900

    스크립트 오류 수정

--- Assets/Scripts/Manager/DarkMuseum.cs
+++ Assets/Scripts/Manager/DarkMuseum.cs
@@ -9,16 +9,22 @@ public class DarkMuseum : MonoBehaviour
     void Start()
     {
         theDM = FindObjectOfType<DialogueManager>();
-        DialogueManager.isMonologue = true;
-        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 1;
-        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 1;
-        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
-        DialogueManager.isRealEnd = false;
+        StartCoroutine(darkMuseum());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator darkMuseum()
+    {
+        DialogueManager.isMonologue = true;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 34;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 34;
+        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
+        DialogueManager.isRealEnd = false;
+        yield return null;
     }
 }
