2b761d827be048d732c50c370d2ac6c60de79ed0
Vimarone <44824410+Vimarone@users.noreply.github.com>
2020-05-18 00:42:41 +0900

    Revert "DeepDarkMuseum 완성까지 다섯발자국"
    This reverts commit c8869227995ee14fe46d3cc04b00440a419e4530.

--- Assets/Scripts/Manager/DarkMuseum.cs
+++ Assets/Scripts/Manager/DarkMuseum.cs
@@ -1,10 +1,8 @@
-﻿using Invector.vCharacterController;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
-using static UnityEngine.Rendering.DebugUI;
 
 public class DarkMuseum : MonoBehaviour
 {
@@ -12,12 +10,7 @@ public class DarkMuseum : MonoBehaviour
     ScanMode sm;
     [SerializeField] VolumeProfile dp;
     [SerializeField] VolumeProfile np;
-    [SerializeField] GameObject update;
-    [SerializeField] GameObject ladder;
     VolumeProfile pp;
-
-    public bool isInteracted = false;
-
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +30,6 @@ public class DarkMuseum : MonoBehaviour
         {
             GameObject.Find("Museum Volume").GetComponent<Volume>().profile = pp;
         }
-        isInteracted = USBnteract.isInteracted;
     }
 
     IEnumerator darkMuseum()
@@ -61,84 +53,34 @@ public class DarkMuseum : MonoBehaviour
 
 
         pp = dp;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = np;
-        yield return new WaitForSeconds(Random.Range(0.1f, 2.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = dp;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         DialogueManager.isMonologue = true;
         gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 36;
         gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 58;
         theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
 
         pp = np;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = dp;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = np;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = dp;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = np;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = dp;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
         pp = np;
         yield return new WaitUntil(() => DialogueManager.isRealEnd);
         DialogueManager.isRealEnd = false;
 
-        GameObject.FindWithTag("USB").GetComponent<MeshRenderer>().enabled = true;
-        GameObject.FindWithTag("USB").GetComponent<BoxCollider>().enabled = true;
-
-        yield return new WaitUntil(() => USBnteract.isInteracted);
-
+        yield return new WaitUntil(() => !USBnteract.isInteracted);
         //UI애니메이션 실행 + 캐릭터 행동 제한
-        DialogueManager.isMonologue = true;
-        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 59;
-        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 63;
-        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
-        update.SetActive(true);
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().horizontalInput = new GenericInput("", "LeftAnalogHorizontal", "Horizontal");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().verticallInput = new GenericInput("", "LeftAnalogVertical", "Vertical");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().jumpInput = new GenericInput("", "X", "X");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().rollInput = new GenericInput("", "B", "B");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().crouchInput = new GenericInput("", "Y", "Y");
-        yield return new WaitUntil(() => DialogueManager.isRealEnd);
-        DialogueManager.isRealEnd = false;
-        yield return new WaitForSeconds(0.4f);
-        pp = dp;
-        yield return new WaitForSeconds(0.4f);
-        pp = np;
-        update.SetActive(false);
-        GameObject.FindWithTag("USB").GetComponent<MeshRenderer>().enabled = false;
-        GameObject.FindWithTag("USB").GetComponent<BoxCollider>().enabled = false;
-
-        DialogueManager.isRealEnd = false;
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().horizontalInput = new GenericInput("Horizontal", "LeftAnalogHorizontal", "Horizontal");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().verticallInput = new GenericInput("Vertical", "LeftAnalogVertical", "Vertical");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().jumpInput = new GenericInput("Space", "X", "X");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().rollInput = new GenericInput("Q", "B", "B");
-        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().crouchInput = new GenericInput("C", "Y", "Y");
-
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
-        pp = np;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
-        pp = dp;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
-        pp = np;
-        yield return new WaitForSeconds(Random.Range(0.1f, 1.6f));
-        pp = dp;
-        //사다리 애니메이션 및 이후 대화
-        ladder.SetActive(true);
-        yield return new WaitForSeconds(1.0f);
-        DialogueManager.isMonologue = true;
-        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 64;
-        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 68;
-        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
-        yield return new WaitUntil(() => DialogueManager.isRealEnd);
-        DialogueManager.isRealEnd = false;
-
-
 
         yield return null;
     }
