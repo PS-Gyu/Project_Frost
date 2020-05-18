864a9e201b0c0e583dc2f440ac592d14fb39fb11
Vimarone <44824410+Vimarone@users.noreply.github.com>
2020-05-19 01:50:04 +0900

    새장 스크립트 추가

---
+++ Assets/Scripts/Manager/BirdInCage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BirdInCage : MonoBehaviour
+{
+    DialogueManager theDM;
+    ScanMode sm;
+    [SerializeField] VolumeProfile dp;
+    [SerializeField] VolumeProfile np;
+
+    VolumeProfile pp;
+
+    public bool isInteracted = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theDM = FindObjectOfType<DialogueManager>();
+        sm = FindObjectOfType<ScanMode>();
+        //StartCoroutine(darkMuseum());
+        pp = dp;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sm.p2 = pp;
+        if (GameObject.Find("Museum Volume").GetComponent<Volume>().profile == sm.p1) { }
+        else
+        {
+            GameObject.Find("Museum Volume").GetComponent<Volume>().profile = pp;
+        }
+    }
+}
