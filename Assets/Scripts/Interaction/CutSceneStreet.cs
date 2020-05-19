6d966881249b9f993474454d74f7b4287a24fa63
Ax1ash <sanggyu96@gmail.com>
2020-05-20 06:07:46 +0900

    바바바밥밥ㅂ밥바바바ㅏ바바ㅏ

---
+++ Assets/Scripts/Interaction/CutSceneStreet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class CutSceneStreet : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public TimelineAsset timeline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cut Scene")
+        {
+            playableDirector.Play();
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
