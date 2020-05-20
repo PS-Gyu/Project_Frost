ddb5a3482597775113161bb6dadd7a37a0fbe5b5
Ax1ash <sanggyu96@gmail.com>
2020-05-20 22:35:30 +0900

    요태까지 날 미행한고야?

---
+++ Assets/Scripts/Interaction/CutSceneStreet2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+
+public class CutSceneStreet2 : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public TimelineAsset timeline;
+
+    public GameObject textPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cut Scene")
+        {
+            playableDirector.Play();
+            //textPanel.SetActive(false);
+            Destroy(textPanel);
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
