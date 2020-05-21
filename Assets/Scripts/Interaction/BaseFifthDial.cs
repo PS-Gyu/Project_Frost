8c7872cc7a92ea84f0fe1c1caacc49263eeb7513
Vimarone <44824410+Vimarone@users.noreply.github.com>
2020-05-22 00:31:43 +0900

    랏씬 제작 완료

---
+++ Assets/Scripts/Interaction/BaseFifthDial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseFifthDial : MonoBehaviour
+{
+    public static bool fifthDial = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!fifthDial)
+            {
+                fifthDial = true;
+            }
+
+        }
+    }
+}
--- Assets/Scripts/Manager/MuseumBase.cs
+++ Assets/Scripts/Manager/MuseumBase.cs
@@ -1,17 +1,30 @@
-﻿using Invector.vCharacterController;
+﻿using Invector;
+using Invector.vCamera;
+using Invector.vCharacterController;
 using Invector.vCharacterController.vActions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class MuseumBase : MonoBehaviour
 {
     [SerializeField] Camera baseCam;
+    [SerializeField] Camera evCam;
     [SerializeField] Camera mainCam;
+    [SerializeField] GameObject alert;
+    [SerializeField] GameObject EV;
+    [SerializeField] GameObject EVPlayer;
     DialogueManager theDM;
+    int num = 0;
     // Start is called before the first frame update
     void Start()
     {
+        DarkMusFirst.firstDial = false;
+        DarkMusSec.secDial = false;
+        DarkMusThird.thirdDial = false;
+        BaseFourthDial.fourthDial = false;
         theDM = FindObjectOfType<DialogueManager>();
         GameObject.FindWithTag("Player").GetComponent<vThirdPersonController>().enabled = false;
         GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().enabled = false;
@@ -36,15 +49,77 @@ public class MuseumBase : MonoBehaviour
 
         yield return new WaitForSeconds(8.5f);
         baseCam.gameObject.SetActive(false);
-        mainCam.gameObject.SetActive(true);
+        //mainCam.gameObject.SetActive(true);
         GameObject.FindWithTag("Player").GetComponent<vThirdPersonController>().enabled = true;
         GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().enabled = true;
         GameObject.FindWithTag("Player").GetComponent<vGenericAction>().enabled = true;
-
-
-
         yield return new WaitUntil(() => DialogueManager.isRealEnd);
         DialogueManager.isRealEnd = false;
+
+        yield return new WaitForSeconds(2.0f);
+        DialogueManager.isMonologue = true;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 3;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 11;
+        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
+        yield return new WaitUntil(() => DialogueManager.isRealEnd);
+        DialogueManager.isRealEnd = false;
+
+        yield return new WaitUntil(() => DarkMusFirst.firstDial);
+        DialogueManager.isMonologue = true;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 12;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 16;
+        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
+        yield return new WaitUntil(() => DialogueManager.isRealEnd);
+        DialogueManager.isRealEnd = false;
+
+        yield return new WaitUntil(() => DarkMusSec.secDial);
+        yield return new WaitForSeconds(3.0f);
+        DialogueManager.isMonologue = true;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 17;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 19;
+        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
+        yield return new WaitUntil(() => DialogueManager.isRealEnd);
+        DialogueManager.isRealEnd = false;
+
+        yield return new WaitForSeconds(4.0f);
+        alert.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        DialogueManager.isMonologue = true;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 20;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 27;
+        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
+        yield return new WaitUntil(() => DialogueManager.isRealEnd);
+        DialogueManager.isRealEnd = false;
+
+        yield return new WaitUntil(() => DarkMusThird.thirdDial);
+        evCam.gameObject.SetActive(true);
+        EV.GetComponent<PlayableDirector>().enabled = true;
+        GameObject.FindWithTag("Player").GetComponent<vThirdPersonController>().enabled = false;
+        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().enabled = false;
+        GameObject.FindWithTag("Player").GetComponent<vGenericAction>().enabled = false;
+        EVPlayer.transform.SetPositionAndRotation(GameObject.FindWithTag("Player").GetComponent<Transform>().position, GameObject.FindWithTag("Player").GetComponent<Transform>().rotation);
+        yield return new WaitForSeconds(10.5f);
+        evCam.gameObject.SetActive(false);
+        GameObject.FindWithTag("Player").transform.SetPositionAndRotation(EVPlayer.transform.position, EVPlayer.transform.rotation);
+        GameObject.FindWithTag("Player").GetComponent<vThirdPersonController>().enabled = true;
+        GameObject.FindWithTag("Player").GetComponent<vThirdPersonInput>().enabled = true;
+        GameObject.FindWithTag("Player").GetComponent<vGenericAction>().enabled = true;
+        
+
+
+        yield return new WaitUntil(() => BaseFourthDial.fourthDial);
+        DialogueManager.isMonologue = true;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.x = 30;
+        gameObject.GetComponent<InteractionEvent>().dialogue.line.y = 39;
+        theDM.ShowDialogue(gameObject.GetComponent<InteractionEvent>().GetDialogue());
+        yield return new WaitUntil(() => DialogueManager.isRealEnd);
+        DialogueManager.isRealEnd = false;
+
+        yield return new WaitUntil(() => BaseFifthDial.fifthDial);
+        gameObject.GetComponent<PlayableDirector>().enabled = true;
+        yield return new WaitForSeconds(8.7f);
+        SceneManager.LoadScene(8);
+
 
         yield return null;
     }
