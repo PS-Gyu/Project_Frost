8fd2945cf3f4c159499080d2dfa9f4a8fb5f75f9
PS_Gyu <59715525+PS-Gyu@users.noreply.github.com>
2020-05-22 05:14:29 +0900

    발소리 추가

---
+++ Assets/Scripts/Interaction/Scene4to5.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene4to5 : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        SceneManager.LoadScene(5);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
